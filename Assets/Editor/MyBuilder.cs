b1fa9d5454d244d5c49cb01eaf0367e1ca92ff95
unsoluble_sugar <unsoluble.sugar@gmail.com>
2016-04-11 16:41:09 +0900

    Androidのapkビルド用スクリプト追加

---
+++ Assets/Editor/MyBuilder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MyBuilder : MonoBehaviour {
+	[UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+	public static void BuildProjectAllSceneAndroid() {
+		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+		string[] allScene = new string[EditorBuildSettings.scenes.Length];
+		int i = 0;
+		foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+			allScene[i] = scene.path;
+			i++;
+		}
+		PlayerSettings.bundleIdentifier = "com.yourcompany.newgame";
+		PlayerSettings.statusBarHidden = true;
+		BuildPipeline.BuildPlayer( allScene,
+			"newgame.apk",
+			BuildTarget.Android,
+			BuildOptions.None
+		);
+	}
+}
